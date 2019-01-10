b652fc27019b1366d693b193ae9266a675a2dab3
MartinPei <2814855093@qq.com>
2019-01-10 18:23:39 +0800

    the eighteenth chapter end, all thing is ok.

---
+++ learning_logs/urls.py
@@ -0,0 +1,12 @@
+"""定义learning_logs的URL模式"""
+
+from django.urls import path
+from . import views
+
+app_name = 'learning_logs'
+
+urlpatterns = [
+	path('', views.index, name='index'),
+	path('topics/', views.topics, name='topics'),
+	path('topics/(?P<topic_id>\d+)/', views.topic, name='topic'),
+]
